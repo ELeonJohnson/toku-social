90990b6cfc7d42ca73d2cde5dd49e61c97306514
ELeonJohnson <enochjohnsonleon@gmail.com>
2018-07-29 22:27:39 -0400

    added more migrations for the database tables

---
+++ database/migrations/1532915539512_settings_schema.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const Schema = use("Schema");
+
+class SettingsSchema extends Schema {
+  up() {
+    this.create("settings", table => {
+      table.increments();
+      table
+        .integer("user_id")
+        .notNullable()
+        .unsigned()
+        .references("id")
+        .inTable("users");
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("settings");
+  }
+}
+
+module.exports = SettingsSchema;
